7f5e2470b7d8afbccb4b08fbaff485926ad98118
Koston Zhuang <koston.zhuang@maiscrm.com>
2024-10-24 10:58:00 +0800

    tool: add water mark

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MyApp from './App.tsx'
-import { App } from 'antd'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MyApp from './App.tsx';
+import { App, Watermark } from 'antd';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App>
-    <MyApp />
+      <Watermark
+        content={'PPG007 tools'}
+        font={{
+          color: 'rgba(103,100,100,0.5)',
+        }}
+      >
+        <MyApp/>
+      </Watermark>
     </App>
   </React.StrictMode>,
-)
+);
